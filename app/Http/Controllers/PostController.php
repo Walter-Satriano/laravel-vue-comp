dd0ce42108a6d6b1aed5487e716d22342da542c7
Walter Satriano <walter.satri@gmail.com>
2019-10-17 17:07:12 +0200

    funzioni index e show

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
@@ -13,7 +14,8 @@ class PostController extends Controller
   */
   public function index()
   {
-    //
+    $posts = Post::all();
+    return view('pages.postIndex', compact('posts'));
   }
 
   /**
@@ -45,7 +47,8 @@ class PostController extends Controller
   */
   public function show($id)
   {
-    //
+    $post = Post::findOrFail($id);
+    return view('pages.postShow', compact('post'));
   }
 
   /**
